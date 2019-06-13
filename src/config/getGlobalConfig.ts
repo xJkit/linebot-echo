a2d44abdf77eab92cf7f71adf9bb17b2e175524f
xJkit <xjkit54780@gmail.com>
2019-06-13 23:14:02 +0800

    Update global config variable settings

--- src/config/getGlobalConfig.ts
+++ src/config/getGlobalConfig.ts
@@ -5,14 +5,9 @@ interface GlobalConfig {
 }
 
 export default function getGlobalConfig() {
-  const config: GlobalConfig = {};
-
-  if (process.env.NODE_ENV === 'production') {
-    config.channelAccessToken = CHANNEL_ACCESS_TOKEN;
-    config.channelSecret = CHANNEL_SECRET;
-  } else {
-    config.channelAccessToken = process.env.channelAccessToken;
-    config.channelSecret = process.env.channelSecret;
-  }
+  const config: GlobalConfig = {
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret,
+  };
   return config;
 }
